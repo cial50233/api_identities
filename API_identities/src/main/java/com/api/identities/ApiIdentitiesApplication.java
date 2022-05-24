41a119a0687b57e0e8d7d0832c09fdc03d0131cc
Cial TRIN <cial50233@gmail.com>
2022-05-24 11:35:32 +0200

    init

---
+++ API_identities/src/main/java/com/api/identities/ApiIdentitiesApplication.java
@@ -0,0 +1,13 @@
+package com.api.identities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIdentitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIdentitiesApplication.class, args);
+	}
+
+}
---
+++ API_identities/src/test/java/com/api/identities/ApiIdentitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.identities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiIdentitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
